34b147d936c5eae76ca99df2c1d4d3a732c398c6
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-01-02 20:26:58 -0500

    Come to a stopping point

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const easel = document.querySelector('#easel');
-easel.addEventListener('mousedown', function () {
-  
-});
+
+function makeGrid (size) {
+  for (i = 0; i < (size**2); i++) {
+    let cell = document.createElement('div');
+    easel.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    easel.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    easel.insertAdjacentElement('beforeend', cell);
+    // cell.style.border = '1px solid rgb(239, 219, 192)';
+    cell.addEventListener('mouseover', function () {
+      cell.style.backgroundColor = 'gray';
+    });
+    easel.appendChild(cell).className = 'easel-unit';
+
+  };
+};
+
+makeGrid(16);
